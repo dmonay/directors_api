4193547807f7bbb42cece43cf90441f3c9ba9f37
dmonay <pavluk73@gmail.com>
2015-01-07 12:53:36 -0500

    initial commit, API functional

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-directors_api
-=============
+# Coding challenge for Livestream
 
-Coding challenge for LIvestream
+1) Create a database in local mongo called "directors_api"
+
+2) To run the Node server, please run
+```
+node server.js
+```
+
+3) API endpoints:
+
+Register
+
+Update - include director's name, fave camera and fave movies
---
+++ server.js
@@ -0,0 +1,85 @@
+var app = require('express')(),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    request = require('request');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost/directors_api');
+
+
+
+app.get('/getall', listAll);
+app.post('/register', register);
+app.post('/update', update);
+app.listen(1436);
+console.log('\x1b[32;1mServer running at http://localhost:1436/\x1b[0m');
+
+
+var Schema = mongoose.Schema;
+var directorsSchema = new Schema({
+    full_name: String,
+    dob: String,
+    favorite_camera: String,
+    favorite_movies: Array
+});
+var directors = mongoose.model('Directors', directorsSchema);
+
+
+function listAll(req, res) {
+    console.log("\x1b[32;1mListAll Request received\x1b[0m");
+    directors.find(function(err, directors) {
+        if (err) return console.log(err);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(directors);
+    });
+}
+
+function register(req, res) {
+    console.log("\x1b[32;1mRegister Request received\x1b[0m");
+    var name, dob, url = req.body.url;
+    var saveDir = new directors();
+
+    request(url, function(err, resp, html) {
+        if (!err) {
+            body = JSON.parse(resp.body);
+            name = body.full_name;
+            dob = body.dob;
+
+            saveDir.full_name = name;
+            saveDir.dob = dob;
+            saveDir.save(function() {
+            	res.setHeader('Content-Type', 'application/json');
+                res.send("You've registered " + name);
+            });
+        }
+    });
+
+}
+
+function update(req, res) {
+    console.log("\x1b[32;1mUpdate Request received\x1b[0m");
+    var name = req.body.name,
+        find_dir = new RegExp(name);
+
+    directors.find({
+        full_name: find_dir
+    }, callback);
+
+    function callback(err, director) {
+        if (err) return console.log(err);
+        var camera = req.body.favorite_camera,
+            fave_movies = req.body.favorite_movies,
+            dir = director[0];
+
+        if (camera) dir.favorite_camera = camera;
+        if (fave_movies) dir.favorite_movies.push(fave_movies);
+        dir.save(function() {
+        	res.setHeader('Content-Type', 'application/json');
+            res.send("You've updated " + name + "'s profile!");
+        });
+    }
+
+}
