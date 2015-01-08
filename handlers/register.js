309cb37a0b839e1801b84f1b1e927a6f0832de17
dmonay <pavluk73@gmail.com>
2015-01-08 01:46:16 -0500

    add error handling to /register and /update handlers

--- handlers/register.js
+++ handlers/register.js
@@ -2,11 +2,32 @@ var request = require('request');
 
 function register(req, res, directors) {
     console.log("\x1b[32;1mRegister Request received\x1b[0m");
-    var name, dob, url = req.body.url;
-    var registerDir = new directors();
 
-    request(url, function(err, resp, html) {
-        if (!err) {
+    function sendError() {
+        var err_msg = {
+            error: {
+                type: "Malformed syntax",
+                message: "Please provide the correct url for the director you wish to register!"
+            }
+        };
+        res.status(400);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(err_msg);
+    }
+
+    // Validate the incoming JSON
+    if (req.body.url) {
+        var name, dob, url = req.body.url,
+            registerDir = new directors();
+        request(url, callback);
+    } else {
+        sendError();
+    }
+
+    // Hit the relevant endpoint on the Livestream API, extract the needed info,
+    // and create a new Director document in mongo
+    function callback(err, resp, html) {
+        if (!err && resp.statusCode == 200) {
             body = JSON.parse(resp.body);
             name = body.full_name;
             dob = body.dob;
@@ -18,8 +39,10 @@ function register(req, res, directors) {
                 res.status(201);
                 res.send("You've registered " + name);
             });
+        } else {
+            sendError();
         }
-    });
+    }
 }
 
 module.exports = register;
--- handlers/update.js
+++ handlers/update.js
@@ -1,30 +1,55 @@
 function update(req, res, directors) {
     console.log("\x1b[32;1mUpdate Request received\x1b[0m");
     var name, find_dir = new RegExp(name);
-    
+
+    function sendError(msg) {
+        var err_msg = {
+            error: {
+                type: "Malformed syntax",
+                message: msg
+            }
+        };
+        res.status(400);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(err_msg);
+    }
+
+    // Validate the incoming JSON
     if (req.body.name) {
         name = req.body.name;
         directors.find({
             full_name: find_dir
         }, callback);
     } else {
-        res.status(400);
-        res.setHeader('Content-Type', 'application/json');
-        res.send("Please provide the director's name!");
+        sendError("Please provide the director's name!");
     }
 
+    // update the favorite_camera and favorite_movies attributes
+    // of the Director document in mongo, if they are provided
     function callback(err, director) {
         if (err) return console.log(err);
         var camera = req.body.favorite_camera,
             fave_movies = req.body.favorite_movies,
             dir = director[0];
 
+        if (!camera && !fave_movies) {
+            sendError("Please provide favorite_camera, favorite_movies, or both.");
+            return;
+        }
+
         if (camera) dir.favorite_camera = camera;
-        if (fave_movies) {
+
+        // if favorite_movies is properly formatter, add every provided movie to
+        // the favorite_movies of the Director document in mongo
+        if (fave_movies && fave_movies.constructor === Array) {
             for (var i = 0; i < fave_movies.length; i++) {
                 dir.favorite_movies.push(fave_movies[i])
             };
+        } else {
+            sendError("favorite_movies must be an array");
+            return;
         }
+
         dir.save(function() {
             res.setHeader('Content-Type', 'application/json');
             res.send("You've updated " + name + "'s profile!");
